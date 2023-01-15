f60966df525147637cb3e926a91b21eaac0c4654
Rinkesh Sante <rinekshsante@gmail>
2023-01-15 21:50:41 +0530

    removed nested routing

--- src/components/routing/app-routes.tsx
+++ src/components/routing/app-routes.tsx
@@ -1,19 +1,14 @@
-import { lazy, Suspense } from "react";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import useUser from "../../hooks/useUser";
+import Dashboard from "../features/dashboard/Dashboard";
 import FeatureDetails from "../features/features/FeatureDetails";
+import ProjectDetails from "../features/projects/ProjectDetails";
+import ProjectForm from "../features/projects/ProjectForm";
+import ProjectList from "../features/projects/ProjectList";
 import CreateForm from "../ui/form/CreateForm";
 import EditForm from "../ui/form/EditForm";
-import Loader from "../ui/Loader";
 import Navbar from "../ui/Navbar";
 import NotFound from "./not-found";
-
-const Dashboard = lazy(() => import("../features/dashboard/Dashboard"));
-const ProjectDetails = lazy(
-  () => import("../features/projects/ProjectDetails")
-);
-const ProjectList = lazy(() => import("../features/projects/ProjectList"));
-const ProjectForm = lazy(() => import("../features/projects/ProjectForm"));
 
 type Props = {};
 
@@ -24,34 +19,32 @@ export default function AppRoutes({}: Props) {
     <BrowserRouter>
       <Navbar />
       <div className="container pt-2 pb-4">
-        <Suspense fallback={<Loader />}>
-          <Routes>
-            <Route path="/" element={<ProtectedRoute />}>
-              <Route index element={<Dashboard />} />
-              <Route path="/project" element={<ProjectList />} />
+        <Routes>
+          <Route path="/" element={<ProtectedRoute />}>
+            <Route index element={<Dashboard />} />
+            <Route path="/project" element={<ProjectList />} />
 
-              <Route path="/project/:id" element={<ProjectDetails />} />
-              <Route
-                path="/project/add"
-                element={<CreateForm FormComponent={ProjectForm} />}
-              />
-              <Route
-                path="/project/edit/:id"
-                element={
-                  <EditForm
-                    FormComponent={ProjectForm}
-                    collectionName="projects"
-                  />
-                }
-              />
+            <Route path="/project/:id" element={<ProjectDetails />} />
+            <Route
+              path="/project/add"
+              element={<CreateForm FormComponent={ProjectForm} />}
+            />
+            <Route
+              path="/project/edit/:id"
+              element={
+                <EditForm
+                  FormComponent={ProjectForm}
+                  collectionName="projects"
+                />
+              }
+            />
 
-              <Route path="/feature/add" element={<ProjectForm />} />
-              <Route path="/feature/:id" element={<FeatureDetails />} />
+            <Route path="/feature/add" element={<ProjectForm />} />
+            <Route path="/feature/:id" element={<FeatureDetails />} />
 
-              <Route path="*" element={<NotFound />} />
-            </Route>
-          </Routes>
-        </Suspense>
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
       </div>
     </BrowserRouter>
   );
